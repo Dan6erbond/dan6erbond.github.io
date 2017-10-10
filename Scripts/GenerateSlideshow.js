aa5022b892e724d1c714faa47497ae8aa777e039
Dan6erbond <32235074+Dan6erbond@users.noreply.github.com>
2017-10-10 17:17:45 +0200

    Slideshow Generator

--- Scripts/GenerateSlideshow.js
+++ Scripts/GenerateSlideshow.js
@@ -5,7 +5,35 @@ xmlhttp.onreadystatechange = function() {
         var myArr = JSON.parse(this.responseText);
 
         for (var i = 0; i < myArr.length; i++) {
-          zusammenfassung = myArr[i];
+          img = myArr[i];
+
+          var container = document.createElement("div");
+          container.classList.add("mySlide fade");
+
+          var numbertext = document.createElement("div");
+          numbertext.classList.add("numbertext");
+          var number = document.createTextNode(i+1 + "/" + myArr.length);
+          numbertext.appendChild(number);
+
+          var slideshowImg = document.createElement("img");
+          slideshowImg.classList.add("collectionSlideshowImage");
+          slideshowImg.src = img;
+
+          var text = document.createElement("div");
+          text.classList.add("text");
+          var caption = document.createTextNode(img.caption);
+          text.appendChild(caption);
+
+          container.appendChild(numbertext);
+          container.appendChild(slideshowImg);
+          container.appendChild(text);
+
+          var slideshow-container = document.getElementsByClassName("slideshow-container")[0];
+          zusammenfassungen.appendChild(container);
+
+          var dot = document.createElement("span");
+          dot.classList.add("dot");
+          dot.onclick = function(){currentSlide(i+1);};
         }
     }
 };
