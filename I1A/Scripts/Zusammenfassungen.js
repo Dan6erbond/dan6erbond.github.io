b7f61ea2626daad32ca0a144c604e94dfc7c5f12
Dan6erbond <32235074+Dan6erbond@users.noreply.github.com>
2017-10-10 12:00:09 +0200

    asdf
    asdf

--- I1A/Scripts/Zusammenfassungen.js
+++ I1A/Scripts/Zusammenfassungen.js
@@ -1,39 +1,34 @@
 function UpdateZusammenfassungen(){
-  var jsonText;
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-          jsonText = this.responseText;
-      }
-  };
-  xmlhttp.open("GET", "/I1A/Zusammenfassungen/Zusammenfassungen.json", true);
-  xmlhttp.send();
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myArr = JSON.parse(this.responseText);
 
-  console.log(jsonText);
-  var zusammenfassungen = JSON.parse(jsonText);
+        for (var i = 0; i < myArr.length; i++) {
+          var placeholder = document.createElement("div");
 
-  for (var i = 0; i < zusammenfassungen.length; i++) {
-    var zusammenfassung = zusammenfassungen[i];
-    var placeholder = document.createElement("div");
+          var title = document.createElement("p");
+          var titleText = document.createTextNode(zusammenfassung);
+          title.appendChild(titleText);
 
-    var title = document.createElement("p");
-    var titleText = document.createTextNode(zusammenfassung);
-    title.appendChild(titleText);
+          var img = document.createElement("img");
+          img.src = "/I1A/Zusammenfassungen/Images/" + zusammenfassung + ".png";
 
-    var img = document.createElement("img");
-    img.src = "/I1A/Zusammenfassungen/Images/" + zusammenfassung + ".png";
+          var downloadButton = document.createElement("a");
+          downloadButton.classList.add("zusammenfassungDownload");
+          downloadButton.href = "I1A/Zusammenfassungen/" + zusammenfassung + ".pdf";
+          var downloadText = document.createTextNode(zusammenfassung);
+          downloadButton.appendChild(downloadText);
 
-    var downloadButton = document.createElement("a");
-    downloadButton.classList.add("zusammenfassungDownload");
-    downloadButton.href = "I1A/Zusammenfassungen/" + zusammenfassung + ".pdf";
-    var downloadText = document.createTextNode(zusammenfassung);
-    downloadButton.appendChild(downloadText);
+          placeholder.appendChild(title);
+          placeholder.appendChild(img);
+          placeholder.appendChild(downloadButton);
 
-    placeholder.appendChild(title);
-    placeholder.appendChild(img);
-    placeholder.appendChild(downloadButton);
-
-    var zusammenfassungen = document.getElementById("Zusammenfassungen");
-    zusammenfassungen.appendChild(placeholder);
-  }
+          var zusammenfassungen = document.getElementById("Zusammenfassungen");
+          zusammenfassungen.appendChild(placeholder);
+        }
+    }
+};
+xmlhttp.open("GET", "/I1A/Zusammenfassungen/Zusammenfassungen.json", true);
+xmlhttp.send();
 }
