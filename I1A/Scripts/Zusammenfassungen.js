6f9809140365fcc39c1df6b591aff9128ce95b3b
Dan6erbond <32235074+Dan6erbond@users.noreply.github.com>
2017-10-10 11:07:55 +0200

    asdf

--- I1A/Scripts/Zusammenfassungen.js
+++ I1A/Scripts/Zusammenfassungen.js
@@ -1,4 +1,24 @@
 function UpdateZusammenfassungen(){
+  var placeholder = document.createElement("div");
+
+  var title = document.createElement("p");
+  var titleText = document.createTextNode("zusammenfassung");
+
+  var img = document.createElement("img");
+  img.src = "/I1A/Zusammenfassungen/Images/" + "zusammenfassung";
+
+  var downloadButton = document.createElement("a");
+  downloadButton.classList.add("zusammenfassungDownload");
+  downloadButton.href = "I1A/Zusammenfassungen/" + "zusammenfassung";
+
+  placeholder.appendChild(title);
+  placeholder.appendChild(img);
+  placeholder.appendChild(downloadButton);
+  title.appendChild(titleText);
+
+  var zusammenfassungen = document.getElementById("Zusammenfassungen");
+  zusammenfassungen.appendChild(placeholder);
+
   const fs = require('fs');
 
   fs.readdir("I1A/Zusammenfassungen", (err, files) => {
