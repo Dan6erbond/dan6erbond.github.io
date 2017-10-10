40d5857698e6792e2d99464d4d38f2eebb5c8b87
Dan6erbond <32235074+Dan6erbond@users.noreply.github.com>
2017-10-10 11:12:44 +0200

    asdf

--- I1A/Scripts/Zusammenfassungen.js
+++ I1A/Scripts/Zusammenfassungen.js
@@ -1,46 +1,27 @@
 function UpdateZusammenfassungen(){
-  var placeholder = document.createElement("div");
+  $(document).ready(function ()
+    {
+        $.get("/I1A/Zusammenfassungen", function(zusammenfassung)
+        {
+          var placeholder = document.createElement("div");
 
-  var title = document.createElement("p");
-  var titleText = document.createTextNode("zusammenfassung");
+          var title = document.createElement("p");
+          var titleText = document.createTextNode(zusammenfassung);
 
-  var img = document.createElement("img");
-  img.src = "/I1A/Zusammenfassungen/Images/" + "zusammenfassung";
+          var img = document.createElement("img");
+          img.src = "/I1A/Zusammenfassungen/Images/" + zusammenfassung;
 
-  var downloadButton = document.createElement("a");
-  downloadButton.classList.add("zusammenfassungDownload");
-  downloadButton.href = "I1A/Zusammenfassungen/" + "zusammenfassung";
+          var downloadButton = document.createElement("a");
+          downloadButton.classList.add("zusammenfassungDownload");
+          downloadButton.href = "I1A/Zusammenfassungen/" + zusammenfassung;
 
-  placeholder.appendChild(title);
-  placeholder.appendChild(img);
-  placeholder.appendChild(downloadButton);
-  title.appendChild(titleText);
+          placeholder.appendChild(title);
+          placeholder.appendChild(img);
+          placeholder.appendChild(downloadButton);
+          title.appendChild(titleText);
 
-  var zusammenfassungen = document.getElementById("Zusammenfassungen");
-  zusammenfassungen.appendChild(placeholder);
-
-  const fs = require('fs');
-  fs.readdir("I1A/Zusammenfassungen/", (err, files) => {
-    files.forEach(zusammenfassung => {
-      var placeholder = document.createElement("div");
-
-      var title = document.createElement("p");
-      var titleText = document.createTextNode(zusammenfassung);
-
-      var img = document.createElement("img");
-      img.src = "/I1A/Zusammenfassungen/Images/" + zusammenfassung;
-
-      var downloadButton = document.createElement("a");
-      downloadButton.classList.add("zusammenfassungDownload");
-      downloadButton.href = "I1A/Zusammenfassungen/" + zusammenfassung;
-
-      placeholder.appendChild(title);
-      placeholder.appendChild(img);
-      placeholder.appendChild(downloadButton);
-      title.appendChild(titleText);
-
-      var zusammenfassungen = document.getElementById("Zusammenfassungen");
-      zusammenfassungen.appendChild(placeholder);
-    });
-  })
+          var zusammenfassungen = document.getElementById("Zusammenfassungen");
+          zusammenfassungen.appendChild(placeholder);
+        });
+    })
 }
