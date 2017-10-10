440cab77d0e5b3018eea8b8d4b33f182be53a0f2
Dan6erbond <32235074+Dan6erbond@users.noreply.github.com>
2017-10-10 11:05:39 +0200

    scan system

---
+++ I1A/Scripts/Zusammenfassungen.js
@@ -0,0 +1,27 @@
+function UpdateZusammenfassungen(){
+  const fs = require('fs');
+
+  fs.readdir("I1A/Zusammenfassungen", (err, files) => {
+    files.forEach(zusammenfassung => {
+      var placeholder = document.createElement("div");
+
+      var title = document.createElement("p");
+      var titleText = document.createTextNode(zusammenfassung);
+
+      var img = document.createElement("img");
+      img.src = "/I1A/Zusammenfassungen/Images/" + zusammenfassung;
+
+      var downloadButton = document.createElement("a");
+      downloadButton.classList.add("zusammenfassungDownload");
+      downloadButton.href = "I1A/Zusammenfassungen/" + zusammenfassung;
+
+      placeholder.appendChild(title);
+      placeholder.appendChild(img);
+      placeholder.appendChild(downloadButton);
+      title.appendChild(titleText);
+
+      var zusammenfassungen = document.getElementById("Zusammenfassungen");
+      zusammenfassungen.appendChild(placeholder);
+    });
+  })
+}
