3aa918352d8afa2d5145eb4853412c0b2228ce02
Dan6erbond <32235074+Dan6erbond@users.noreply.github.com>
2017-10-10 17:32:49 +0200

    small fix

--- Scripts/GenerateSlideshow.js
+++ Scripts/GenerateSlideshow.js
@@ -1,4 +1,4 @@
-function GenerateSlideshow)(jsonPath){
+function GenerateSlideshow(jsonPath){
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
