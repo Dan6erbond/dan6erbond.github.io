54f06d721aba42991380822f810f761a9cf9b627
Dan6erbond <32235074+Dan6erbond@users.noreply.github.com>
2017-10-10 11:53:24 +0200

    asdf

--- I1A/Scripts/Zusammenfassungen.js
+++ I1A/Scripts/Zusammenfassungen.js
@@ -1,36 +1,39 @@
 function UpdateZusammenfassungen(){
+  var jsonText;
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-          var zusammenfassungen = JSON.parse(this.responseText);
-          for (var i = 0; i < zusammenfassungen.length; i++) {
-            var zusammenfassung = zusammenfassungen[i];
-            console.log(zusammenfassung);
-            console.log(zusammenfassungen);
-            var placeholder = document.createElement("div");
-
-            var title = document.createElement("p");
-            var titleText = document.createTextNode(zusammenfassung);
-            title.appendChild(titleText);
-
-            var img = document.createElement("img");
-            img.src = "/I1A/Zusammenfassungen/Images/" + zusammenfassung + ".png";
-
-            var downloadButton = document.createElement("a");
-            downloadButton.classList.add("zusammenfassungDownload");
-            downloadButton.href = "I1A/Zusammenfassungen/" + zusammenfassung + ".pdf";
-            var downloadText = document.createTextNode(zusammenfassung);
-            downloadButton.appendChild(downloadText);
-
-            placeholder.appendChild(title);
-            placeholder.appendChild(img);
-            placeholder.appendChild(downloadButton);
-
-            var zusammenfassungen = document.getElementById("Zusammenfassungen");
-            zusammenfassungen.appendChild(placeholder);
-          }
+          jsonText = this.responseText;
       }
   };
   xmlhttp.open("GET", "/I1A/Zusammenfassungen/Zusammenfassungen.json", true);
   xmlhttp.send();
+
+  console.log(jsonText);
+  var zusammenfassungen = JSON.parse(jsonText);
+
+  for (var i = 0; i < zusammenfassungen.length; i++) {
+    var zusammenfassung = zusammenfassungen[i];
+    var placeholder = document.createElement("div");
+
+    var title = document.createElement("p");
+    var titleText = document.createTextNode(zusammenfassung);
+    title.appendChild(titleText);
+
+    var img = document.createElement("img");
+    img.src = "/I1A/Zusammenfassungen/Images/" + zusammenfassung + ".png";
+
+    var downloadButton = document.createElement("a");
+    downloadButton.classList.add("zusammenfassungDownload");
+    downloadButton.href = "I1A/Zusammenfassungen/" + zusammenfassung + ".pdf";
+    var downloadText = document.createTextNode(zusammenfassung);
+    downloadButton.appendChild(downloadText);
+
+    placeholder.appendChild(title);
+    placeholder.appendChild(img);
+    placeholder.appendChild(downloadButton);
+
+    var zusammenfassungen = document.getElementById("Zusammenfassungen");
+    zusammenfassungen.appendChild(placeholder);
+  }
 }
