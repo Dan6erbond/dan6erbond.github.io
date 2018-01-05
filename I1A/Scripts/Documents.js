05634cc6656e4b60488e17fd5a390043b6d1d9aa
Dan6erbond <32235074+Dan6erbond@users.noreply.github.com>
2018-01-05 12:13:31 +0100

    added schoolhelper apk

--- I1A/Scripts/Documents.js
+++ I1A/Scripts/Documents.js
@@ -38,6 +38,8 @@ function GenerateDocument(element, documentName, documentPath) {
     thumbnail.src = 'Documents/Images/ZIP Thumbnail.png';
   if (documentPath.indexOf('pptx') >= 0)
     thumbnail.src = 'Documents/Images/PowerPoint Thumbnail.png';
+  if (documentPath.indexOf('apk') >= 0)
+    thumbnail.src = 'Images/SchoolHelper.png';
   thumbnail.alt = documentName;
   thumbnail.classList.add('documentThumbnail');
 
