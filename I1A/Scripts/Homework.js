f78121b4301866a9f87a31e0c07a54250bd8730a
Dan6erbond <32235074+Dan6erbond@users.noreply.github.com>
2018-02-25 13:23:04 +0100

    loading homework should work now

--- I1A/Scripts/Homework.js
+++ I1A/Scripts/Homework.js
@@ -8,7 +8,7 @@ function UpdateHomework() {
     if (this.readyState == 4 && this.status == 200) {
       var myArr = JSON.parse(this.responseText)[''];
       for (var i = 0; i < myArr.length; i++) {
-        var table = document.findElementById('HomeworkTable');
+        var table = document.getElementById('homeworkTable');
         var tableRow = document.createElement('tr');
         var date = document.createElement('td');
         var subject = document.createElement('td');
@@ -16,13 +16,13 @@ function UpdateHomework() {
         date.innerHTML = myArr[i].datum;
         subject.innerHTML = myArr[i].fach;
         job.innerHTML = myArr[i].aufgabe;
-        tablerow.appendChild(date);
-        tablerow.appendChild(subject);
-        tablerow.appendChild(job);
+        tableRow.appendChild(date);
+        tableRow.appendChild(subject);
+        tableRow.appendChild(job);
         table.appendChild(tableRow);
       }
     }
   };
-  xmlhttp.open("GET", "http://dan6erbond.bplaced.net/Hausaufgaben.json", true);
+  xmlhttp.open("GET", "http://cors.io/?http://dan6erbond.bplaced.net/Hausaufgaben.json", true);
   xmlhttp.send();
 }
