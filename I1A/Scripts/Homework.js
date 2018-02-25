367f5c0f9a51a4fd21b5492c23c9546c4073cce0
Dan6erbond <32235074+Dan6erbond@users.noreply.github.com>
2018-02-25 14:25:30 +0100

    fixed loading homework now

--- I1A/Scripts/Homework.js
+++ I1A/Scripts/Homework.js
@@ -6,7 +6,7 @@ function UpdateHomework() {
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      var myArr = JSON.parse(this.responseText)[''];
+      var myArr = JSON.parse(this.responseText);
       for (var i = 0; i < myArr.length; i++) {
         var table = document.getElementById('homeworkTable');
         var tableRow = document.createElement('tr');
