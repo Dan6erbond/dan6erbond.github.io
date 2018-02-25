438cce3b16fc0cc8d454b7c3df7ef6c7726dee28
Dan6erbond <32235074+Dan6erbond@users.noreply.github.com>
2018-02-25 13:53:30 +0100

    fixed loading again

--- I1A/Scripts/Homework.js
+++ I1A/Scripts/Homework.js
@@ -8,11 +8,11 @@ function UpdateHomework() {
     if (this.readyState == 4 && this.status == 200) {
       var myArr = JSON.parse(this.responseText)[''];
       for (var i = 0; i < myArr.length; i++) {
-        var table = document.getElementById('homeworkTable').childNodes[0];
-        var tableRow = table.insertRow(tableRef.rows.length);
-        var date = tableRow.insertCell(0);
-        var subject = tableRow.insertCell(1);
-        var job = tableRow.insertCell(2);
+        var table = document.getElementById('homeworkTable').getElementsByTagName('tbody')[0];
+        var tableRow = table.insertRow();
+        var date = tableRow.insertCell();
+        var subject = tableRow.insertCell();
+        var job = tableRow.insertCell();
         date.innerHTML = myArr[i].datum;
         subject.innerHTML = myArr[i].fach;
         job.innerHTML = myArr[i].aufgabe;
