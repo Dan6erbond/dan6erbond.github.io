8bda0431cd38418040f88b9cfe5032c8698a256e
Dan6erbond <32235074+Dan6erbond@users.noreply.github.com>
2018-03-18 13:23:09 +0100

    adding homework from website possible now!!!

--- I1A/Scripts/Homework.js
+++ I1A/Scripts/Homework.js
@@ -1,3 +1,5 @@
+var homeworkArray;
+
 setTimeout(function() {
   UpdateHomework();
 }, 1);
@@ -6,19 +8,40 @@ function UpdateHomework() {
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      var myArr = JSON.parse(this.responseText);
-      for (var i = 0; i < myArr.length; i++) {
-        var table = document.getElementById('homeworkTable').getElementsByTagName('tbody')[0];
-        var tableRow = table.insertRow();
-        var date = tableRow.insertCell();
-        var subject = tableRow.insertCell();
-        var job = tableRow.insertCell();
-        date.innerHTML = myArr[i].datum;
-        subject.innerHTML = myArr[i].fach;
-        job.innerHTML = myArr[i].aufgabe;
+      homeworkArray = JSON.parse(this.responseText);
+      for (var i = 0; i < homeworkArray.length; i++) {
+        AddHomeworkToTable(homeworkArray[i].datum, homeworkArray[i].fach, homeworkArray[i].aufgabe);
       }
     }
   };
   xmlhttp.open("GET", "https://api.myjson.com/bins/1h8sp7", true);
   xmlhttp.send();
+}
+
+function AddHomeworkToTable(a, b, c){
+  var table = document.getElementById('homeworkTable').getElementsByTagName('tbody')[0];
+  var tableRow = table.insertRow();
+  var date = tableRow.insertCell();
+  var subject = tableRow.insertCell();
+  var job = tableRow.insertCell();
+  date.innerHTML = a;
+  subject.innerHTML = b;
+  job.innerHTML = c;
+}
+
+function AddHomework() {
+  var homework = new Object();
+  var date = new Date(document.getElementById("homeworkDate").value + 'T00:00');
+  if(isNaN(date.getDate()) || isNaN(date.getMonth()) || isNaN(date.getFullYear()) || document.getElementById("homeworkJob").value == "")
+    return;
+  var dateString = date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear();
+  homework.datum = dateString;
+  homework.fach = document.getElementById("homeworkSubject").value;
+  homework.aufgabe = document.getElementById("homeworkJob").value;
+  AddHomeworkToTable(homework.datum, homework.fach, homework.aufgabe);
+  homeworkArray.push(homework);
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("PUT", "https://api.myjson.com/bins/1h8sp7", true);
+  xmlhttp.setRequestHeader('Content-type','application/json; charset=utf-8');
+  xmlhttp.send(JSON.stringify(homeworkArray));
 }
