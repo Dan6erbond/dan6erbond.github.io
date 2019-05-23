bfa9cd50d7c2ce89d92d20fd3892e41f08e7afa3
Dan6erbond <moravrav@gmail.com>
2019-05-23 14:41:44 +0200

    contact form

---
+++ Scripts/contact.js
@@ -0,0 +1,29 @@
+var elasticToken = "34753042-b11b-4a1d-80e2-da50b4387c4e";
+// var gmailToken = "3958110c-d50d-4b40-af30-65f9d29079a0";
+var gmailToken = "cb72fe37-0762-40a2-911d-1a68e15e7d55";
+
+function send(){
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+
+  if (email.includes("@") && email.split("@")[1].includes(".")){
+    Email.send({
+        SecureToken: gmailToken,
+        To : 'moravrav@gmail.com',
+        From : "moravrav@gmail.com",
+        Subject : "New message by " + name,
+        Body : "<b>Message:</b><br>" + message + "<br><br><b>Email:</b> " + email
+    }).then(
+      function after(message){
+        if (message == "OK"){
+          alert("Message successfully sent! Thanks for contacting me!");
+        } else {
+          alert("Message not sent, I'm working on fixing that!");
+        }
+      }
+    );
+  } else {
+    alert("Please enter a valid E-Mail address!");
+  }
+}
