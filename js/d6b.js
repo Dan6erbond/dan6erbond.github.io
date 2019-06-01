82548e4e76d1524b552b5fc3a2be188e2ef85d79
Dan6erbond <moravrav@gmail.com>
2019-06-01 15:01:45 +0200

    fixed links, renamed folder, added copy-link, created wiki layout

--- js/d6b.js
+++ js/d6b.js
@@ -14,3 +14,12 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+function copyToClipboard(str){
+  const el = document.createElement('textarea');
+  el.value = str;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+}
