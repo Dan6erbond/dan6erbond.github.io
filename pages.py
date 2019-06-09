0b4db80891f861f20183a50db7b9974f8edd2040
Dan6erbond <moravrav@gmail.com>
2019-06-09 10:33:23 +0200

    SMART docs code

--- pages.py
+++ pages.py
@@ -2,51 +2,53 @@ import os
 import json
 from bs4 import BeautifulSoup
 
-pages = list()
+subFolders = True
 
-folders = [f for f in os.listdir('.') if os.path.isdir(f)]
+while subFolders:
+    subFolders = False
+    
 
-# print(folders)
+folders = ["."]
+folders.extend([f for f in os.listdir('.') if os.path.isdir(f)])
+
+print(folders)
 
 files = list()
 
 for _f in folders:
-    # print(_f)
-    _files = [_f + "/" + f for f in os.listdir(_f) if os.path.isfile(_f + "/" + f)]
-    # print(_files)
+    _files = [_f + "/" + f for f in os.listdir(_f) if os.path.isfile(_f + "/" + f) and f.endswith(".html")]
     files.extend(_files)
 
-# print(files)
+pages = list()
 
 for f in files:
-    if f.endswith(".html"):
-        with open(f, encoding="utf8") as f:
-            page = dict()
+    with open(f, encoding="utf8") as f:
+        page = dict()
 
-            soup = BeautifulSoup(f.read(), 'html.parser')
+        soup = BeautifulSoup(f.read(), 'html.parser')
 
-            if not hasattr(soup, "title") or not hasattr(soup.title, "string"):
+        if not hasattr(soup, "title") or not hasattr(soup.title, "string"):
+            continue
+
+        page["key"] = soup.title.string
+
+        add = True
+        for meta in soup.find_all("meta"):
+            if meta.get("name") is None:
                 continue
 
-            page["key"] = soup.title.string
+            if meta.get("name").lower() == "index" and meta.get("content").lower() == "false":
+                add = False
+                break
 
-            add = True
-            for meta in soup.find_all("meta"):
-                if meta.get("name") is None:
-                    continue
+            if meta.get("name").lower() == "description":
+                page["description"] = meta.get("content")
+            if meta.get("name").lower() == "keywords":
+                page["tags"] = [s.strip() for s in meta.get("content").split(",")]
 
-                if meta.get("name").lower() == "index" and meta.get("content").lower() == "false":
-                    add = False
-                    break
+        page["url"] = f.name
 
-                if meta.get("name").lower() == "description":
-                    page["description"] = meta.get("content")
-                if meta.get("name").lower() == "keywords":
-                    page["tags"] = [s.strip() for s in meta.get("content").split(",")]
-
-            page["url"] = f.name
-
-            if add: pages.append(page)
+        if add: pages.append(page)
 
 with open("pages.json", "w+", encoding="utf8") as f:
     f.write(json.dumps(pages, indent=4))
