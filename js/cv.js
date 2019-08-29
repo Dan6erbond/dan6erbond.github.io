159f7645174ff5869b37f7c049d678ea708bfa08
Dan6erbond <moravrav@gmail.com>
2019-08-29 15:40:51 +0200

    cv script

--- js/cv.js
+++ js/cv.js
@@ -1,26 +1,21 @@
-function isScrolledIntoView(elem) {
-  var docViewTop = $(window).scrollTop();
-  var docViewBottom = docViewTop + $(window).height();
-  var elemTop = $(elem).offset().top;
-  var elemBottom = elemTop; //+ $(elem).height()
-  return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom) && (elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-}
-
-function isNotVisible() {
+function isVisible(elem) {
   var docViewTop = $(window).scrollTop();
   var docViewBottom = docViewTop + $(window).height();
   var elemTop = $(elem).offset().top;
   var elemBottom = elemTop + $(elem).height();
-  return docViewBottom < elemTop || docViewTop > elemBottom;
+  return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom) && (elemBottom <= docViewBottom) && (elemTop >= docViewTop));
 }
 
 $(window).scroll(function() {
-  if (isScrolledIntoView($('#skills'))) {
+  if (isVisible($('#skills'))) {
     $(".skill").each(function() {
       $(this).children("div").children("div").removeClass("collapsed");
+      console.log("visible");
     });
-  }
-  if (isNotVisible($('#skills'))) {
-    alert("invisible");
-  }
+  } /* else {
+    $(".skill").each(function() {
+      $(this).children("div").children("div").addClass("collapsed");
+      console.log("invisible");
+    });
+  } */
 });
