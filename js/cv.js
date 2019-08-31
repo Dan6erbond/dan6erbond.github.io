26b40344dc20aa63ad716d2630e0f800bc5a5282
Dan6erbond <moravrav@gmail.com>
2019-08-31 10:34:06 +0200

    mobile improvements

--- js/cv.js
+++ js/cv.js
@@ -2,7 +2,7 @@ function isVisible(elem) {
   var docViewTop = $(window).scrollTop();
   var docViewBottom = docViewTop + $(window).height();
   var elemTop = $(elem).offset().top;
-  var elemBottom = elemTop + $(elem).height();
+  var elemBottom = elemTop; // + $(elem).height();
   return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom) && (elemBottom <= docViewBottom) && (elemTop >= docViewTop));
 }
 
@@ -10,7 +10,7 @@ $(window).scroll(function() {
   if (isVisible($('#skills'))) {
     $(".skill").each(function() {
       $(this).children("div").children("div").removeClass("collapsed");
-      console.log("visible");
+      // console.log("visible");
     });
   } /* else {
     $(".skill").each(function() {
