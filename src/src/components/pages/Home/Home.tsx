5c6c730b7400c01f46c7add29253996351fbedba
Dan6erbond <moravrav@gmail.com>
2020-05-08 20:20:18 +0200

    Add project cards to home page

--- src/src/components/pages/Home/Home.tsx
+++ src/src/components/pages/Home/Home.tsx
@@ -5,6 +5,8 @@ import Banner from "./Banner";
 
 import './Home.scss';
 import ScrollDown from "./ScrollDown";
+import {Link} from "react-router-dom";
+import {Card, Col, Container, Row} from "react-bootstrap";
 
 export default function Home() {
     const aboutRef = React.useRef(null);
@@ -28,15 +30,90 @@ export default function Home() {
             <div ref={aboutRef}>
                 <div className="profile-img"/>
                 <br/>
-                <p style={{margin: 0}}>
-                    I'm a motivated engineering student with a passion for computer sciences and digital media.
-                    Seeking to work on creating reliable systems and interesting technology.
-                    Created many systems used by others such as the Fuzzle search algorithm.
-                </p>
+                <Container fluid="md">
+                    <Row>
+                        <p style={{margin: 0}}>
+                            I'm a motivated engineering student with a passion for computer sciences and digital media.
+                            Seeking to work on creating reliable systems and interesting technology.
+                            Created many systems used by others such as the Fuzzle search algorithm.
+                        </p>
+                    </Row>
+                </Container>
                 <Timeline/>
                 <ScrollDown targetRef={projectsRef} label="Projects"/>
             </div>
-            <div ref={projectsRef}>
+            <br/>
+            <div ref={projectsRef} style={{overflow: 'auto'}}>
+                <Container fluid="md">
+                    <Row>
+                        <Col>
+                            <Card>
+                                <Card.Body>
+                                    <Container>
+                                        <Row>
+                                            <Col sm={12} md={4}>
+                                                <img style={{width: '90%', minWidth: '150px', maxWidth: '300px'}} alt="IDPA" height="auto"
+                                                     src="/res/img/projects/idpa.jpg"/>
+                                            </Col>
+                                            <Col sm={12} md={8}>
+                                                <h4>Interdisciplinary Project Work</h4>
+                                                <p>Project about the usage of IT Tools and Software in Company and Private
+                                                    Workflows</p>
+                                                <Link className="btn btn-outline-primary flat-button" to="/projects/idpa">Read
+                                                    More</Link>
+                                            </Col>
+                                        </Row>
+                                    </Container>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                    <br/>
+                    <Row>
+                        <Col xs={12} md={6}>
+                            <Card>
+                                <Card.Body>
+                                    <Container>
+                                        <Row>
+                                            <Col sm={12} md={4}>
+                                                <img style={{width: '90%', minWidth: '150px', maxWidth: '300px'}} alt="IDPA" height="auto"
+                                                     src="/res/img/projects/idpa.jpg"/>
+                                            </Col>
+                                            <Col sm={12} md={8}>
+                                                <h4>Interdisciplinary Project Work</h4>
+                                                <p>Project about the usage of IT Tools and Software in Company and Private
+                                                    Workflows</p>
+                                                <Link className="btn btn-outline-primary flat-button" to="/projects/idpa">Read
+                                                    More</Link>
+                                            </Col>
+                                        </Row>
+                                    </Container>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col xs={12} md={6}>
+                            <Card>
+                                <Card.Body>
+                                    <Container>
+                                        <Row>
+                                            <Col sm={12} md={4}>
+                                                <img style={{width: '90%', minWidth: '150px', maxWidth: '300px'}} alt="IDPA" height="auto"
+                                                     src="/res/img/projects/idpa.jpg"/>
+                                            </Col>
+                                            <Col sm={12} md={8}>
+                                                <h4>Interdisciplinary Project Work</h4>
+                                                <p>Project about the usage of IT Tools and Software in Company and Private
+                                                    Workflows</p>
+                                                <Link className="btn btn-outline-primary flat-button" to="/projects/idpa">Read
+                                                    More</Link>
+                                            </Col>
+                                        </Row>
+                                    </Container>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         </React.Fragment>
     );
