b194fff2f645c5d771754de229cc1c85a6bcc4fd
Dan6erbond <moravrav@gmail.com>
2020-05-09 20:57:20 +0200

    Write-up on IDPA page
     - Add research, summary, and conclusion texts on idpa page
     - Add graph with survey response data on idpa page
     - Fix style of container in idpa page spacing to following text

--- src/src/components/pages/Home/Projects.tsx
+++ src/src/components/pages/Home/Projects.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Card, Col, Container, Row} from "react-bootstrap";
+import {Col, Container, Row} from "react-bootstrap";
 import {Link} from "react-router-dom";
 import VizSensor from "react-visibility-sensor";
 import {motion} from "framer-motion";
--- src/src/components/pages/Projects/IDPA/IDPA.tsx
+++ src/src/components/pages/Projects/IDPA/IDPA.tsx
@@ -7,6 +7,7 @@ import ScrollDown from "../../../ScrollDown/ScrollDown";
 import {faArrowAltCircleDown, faHammer} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import Banner from "../../../Banner/Banner";
+import {SoftwareAspectsGraph} from "./Graphs";
 
 export default function IDPA() {
     const [textVisible, setTextVisible] = React.useState<boolean>(false);
@@ -99,13 +100,13 @@ export default function IDPA() {
                         }} transition={{duration: 0.6}}>
                             <h3>Topic</h3>
                             <p>
-                                The topic discussed in my project is the usage of IT tools and various software in
-                                company environments. As research, specific to company environments can only reliably
-                                done at the intern company, though, the first period focused on the usage for school
+                                The topic discussed in this project is the usage of IT tools and various software in
+                                company environments. As research, specific to company environments, can only reliably
+                                be done at the intern company, though, the first period focused on the usage for school
                                 and private workflows.
                                 <br/>
                                 <br/>
-                                There were three key questions to be answered by the end of the research:
+                                There were three key questions to be answered by the end of the examination:
                                 <ul>
                                     <li>
                                         How can workflows be visualized with IT tools in company, private and school
@@ -123,15 +124,62 @@ export default function IDPA() {
                             </p>
                             <h3>Research</h3>
                             <p>
-                                <FontAwesomeIcon icon={faHammer}/> Work in progress!
+                                To create a baseline of software and tools to examine, a set of visualisation and
+                                automation programs were found and compared. Additionally, some quantitative research
+                                was done with the results of a survey that received over 120 responses from multiple
+                                demographics in IT- and non-IT related fields.
+                                <br/>
+                                <br/>
+                                The chosen tools for the visualisation were: <a href="https://www.visual-paradigm.com/"
+                                                                                target="_blank"
+                                                                                rel="noopener noreferrer">Visual
+                                Paradigm</a>, <a href="https://www.diagrams.net/" target="_blank"
+                                                 rel="noopener noreferrer">Draw.io</a>, <a
+                                href="https://www.microsoft.com/en/microsoft-365/visio/flowchart-software"
+                                target="_blank" rel="noopener noreferrer">Microsoft Visio</a> and <a
+                                href="https://www.umlet.com/" target="_blank" rel="noopener noreferrer">UMLet</a>.
+                                <br/>
+                                <br/>
+                                In the portion containing field research done by using the given tools and visualising,
+                                as well as automating, a set of workflows, a multitude of software were used based on
+                                the use-case including self-made Python scripts, and <a
+                                href="https://emea.flow.microsoft.com/en-us/" target="_blank"
+                                rel="noopener noreferrer">Microsoft Power Automate</a>.
                             </p>
                             <h3>Summary</h3>
                             <p>
-                                <FontAwesomeIcon icon={faHammer}/> Work in progress!
+                                Besides evaluating the survey responses, which showed in particular what kind of tasks
+                                are common and can be automated as well as what kind of software users prefer, workflows
+                                were visualized and automated with the right set of tools to analyze their effectiveness
+                                and overall practically in comparison to traditional methods.
+                                <br/>
+                                <br/>
+                                This graph from the survey responses shows which aspects of a software users find most
+                                important:
+                                <br/>
+                                <br/>
+                                <div style={{width: '100%', height: '45vh'}}>
+                                    <SoftwareAspectsGraph/>
+                                </div>
                             </p>
                             <h3>Conclusion</h3>
                             <p>
-                                <FontAwesomeIcon icon={faHammer}/> Work in progress!
+                                Among the tools examined to create visualizations of workflows, Microsoft Visio and
+                                UMLet seem the most suitable. Visual Paradigm and Draw.io are alternatives that offer
+                                a compelling set of features, but don't scale with the overall capabilities of Visio
+                                or UMLet's simplicity. Additionally, Visual Paradigm is financially less feasible for
+                                smaller businesses, already utilizing the Microsoft Office Suite, or those looking
+                                for an open source option. UMLet also comes ahead in terms of its widespread use, as
+                                it's been dominant in the field of software engineering and database design for quite
+                                some time now.
+                                <br/>
+                                <br/>
+                                The conclusion for automation is much more complicated. While tools such as Zapier and
+                                Microsoft Power Automate are highly capable and work very reliable in the environment
+                                they were designed for, they don't scale well with the individual needs of a user and
+                                thus get edged out by self-made scripts and programs consistently. Additionally, any
+                                self-made script can be made open source and improved upon in rapid succession, whereas
+                                updates to larger platforms only come in bulk.
                             </p>
                         </motion.div>
                     </Container>
