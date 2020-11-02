eded7a1124013874b78363011d8a0ae2f4c9bcd6
Dan6erbond <moravrav@gmail.com>
2020-11-02 20:26:51 +0100

    Remove redundant links and add meme list in store

--- wall-of-memes/src/src/store/index.js
+++ wall-of-memes/src/src/store/index.js
@@ -5,11 +5,16 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    memes: [
+      {
+        url:
+          "https://www.reddit.com/r/wholesomememes/comments/dv40yz/lets_teach_the_youngins/",
+        img:
+          "https://preview.redd.it/78ul9vera6y31.jpg?width=960&crop=smart&auto=webp&s=652f9a2deb65d443fa24263102298102b4e805a6",
+      },
+    ],
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  mutations: {},
+  actions: {},
+  modules: {},
+});
