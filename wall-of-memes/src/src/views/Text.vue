5f6f1f6c7a62dd80bc24a634475400b6bff291fe
Dan6erbond <moravrav@gmail.com>
2020-11-03 09:11:23 +0100

    Add letter text and use Bootstrap `display-` styling class

--- wall-of-memes/src/src/views/Text.vue
+++ wall-of-memes/src/src/views/Text.vue
@@ -3,9 +3,40 @@
     <vue-headful title="Cate B-Day 2020 | Text" />
     <div class="page">
       <p>
-        <span class="text-center d-block top">Cate,</span>
+        <span class="text-center d-block display-4">Cate,</span>
         <br />
-        <span class="main-text">Text.</span>
+        <span class="main-text">
+          <span>
+            I know I said this over on Reddit already but you truly are one of
+            the most amazing people I've ever met. It would literally be
+            impossible to list all the things I love about you, but you're
+            genuinely the best friend I've ever had and I couldn't ask for more.
+          </span>
+          <br />
+          <br />
+          <span>
+            I know this year has been incredibly messy and hard but we've
+            managed to communicate and stay by each other's side through all of
+            it and it means the world to me. I know someday we'll be able to
+            look back at all of this and see how much it's taught us, and for
+            now we just have to pull through. But I know with you by my side
+            I'll always manage.
+          </span>
+          <br />
+          <br />
+          <span>
+            At the moment though I hope you enjoyed the memes and Reddit posts
+            I've been collecting and have an amazing birthday!!! 😊
+          </span>
+          <br />
+          <span class="display-3 text-center d-block mt-4">🎂</span>
+          <br />
+          <span class="display-4 text-right d-block mr-4">
+            Love,
+            <br />
+            Ravi
+          </span>
+        </span>
       </p>
     </div>
   </div>
@@ -26,9 +57,5 @@
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
-
-  .top {
-    font-size: 1.5em;
-  }
 }
 </style>
