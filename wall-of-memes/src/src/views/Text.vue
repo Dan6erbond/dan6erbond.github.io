063a2518ea1258addfad146cd53e0fbf5fe09349
Dan6erbond <moravrav@gmail.com>
2020-11-03 02:03:47 +0100

    Add text component with page background

--- wall-of-memes/src/src/views/Text.vue
+++ wall-of-memes/src/src/views/Text.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="text">
+  <div class="text p-3 pb-4">
     <vue-headful title="Cate B-Day 2020 | Text" />
+    <div class="page">
+      <p>
+        <span class="text-center d-block top">Cate,</span>
+        <br />
+        <span class="main-text">Text.</span>
+      </p>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css2?family=Itim&display=swap");
+
+.page {
+  color: navy;
+  font-size: 1.65em;
+  text-align: left;
+  font-family: "Itim", cursive;
+  border: solid 10px #b11226;
+  min-height: 75vh;
+  padding: 20px;
+  background-image: url("../assets/img/white-paper-canvas-texture-background-design-backdrop-overlay-design_1962-68.jpg");
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+
+  .top {
+    font-size: 1.5em;
+  }
+}
+</style>
