3aaed3c6177d6b7d1cf36283a3a01d1dbea7cc3c
Dan6erbond <moravrav@gmail.com>
2020-11-03 02:02:58 +0100

    Add Bootstrap Vue SASS overrides

--- wall-of-memes/src/src/plugins/bootstrap-vue.js
+++ wall-of-memes/src/src/plugins/bootstrap-vue.js
@@ -1,7 +1,5 @@
-import Vue from 'vue'
+import BootstrapVue from "bootstrap-vue";
+import Vue from "vue";
+import "../assets/css/main.scss";
 
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
